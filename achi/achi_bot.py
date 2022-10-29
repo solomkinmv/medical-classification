d429cdcd92218a701db045c0d8ca25d2341ff53d
Maksym Solomkin <maksym@solomk.in>
2022-10-29 20:58:41 +0300

    Add inline mode and fallback handler

--- achi/achi_bot.py
+++ achi/achi_bot.py
@@ -1,22 +1,47 @@
 import logging
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 import os
+
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+
 async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text_caps = ' '.join(context.args).upper()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(os.environ['TOKEN']).build()
@@ -29,5 +54,12 @@ if __name__ == '__main__':
 
     caps_handler = CommandHandler('caps', caps)
     application.add_handler(caps_handler)
+
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    application.add_handler(inline_caps_handler)
+
+    # Other handlers
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    application.add_handler(unknown_handler)
 
     application.run_polling()
