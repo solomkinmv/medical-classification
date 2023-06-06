e8ce4365446059dca9dfce91ef1fb884094f91bc
Maksym Solomkin <maksym@solomk.in>
2023-06-06 22:50:04 +0200

    Initial iOS Commit

---
+++ Green ACHI/Green ACHI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Green ACHI
+//
+//  Created by Maksym Solomkin on 06.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
